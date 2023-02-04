e8a86aa8aa3c7f4f4f0a200ef643a4281eefa253
Gustav2 <gustavnybro@protonmail.com>
2023-02-04 16:16:30 +0100

    cleanup in CVApp

--- CVApp/classifyobject.py
+++ CVApp/classifyobject.py
@@ -16,24 +16,22 @@ from camera4kivy import Preview
 from object_detection.object_detector import ObjectDetector
 from object_detection.object_detector import ObjectDetectorOptions
 
+
 class ClassifyObject(Preview):
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.classified = []
         num_threads = 4
-        enable_edgetpu = False    ## Change this for Coral Accererator
         if platform == 'android':
             model = 'object_detection/model.tflite'
-        elif enable_edgetpu:
-            model = 'object_detection/efficientdet_lite0_edgetpu.tflite'
         else:
             model = 'object_detection/efficientdet_lite0.tflite'
         options = ObjectDetectorOptions(
-            num_threads = 4,
-            score_threshold = 0.5,
-            max_results = 3,
-            enable_edgetpu = enable_edgetpu)
+            num_threads=4,
+            score_threshold=0.5,
+            max_results=3,
+            enable_edgetpu=False)
         self.detector = ObjectDetector(model_path=model, options=options)
         # Get the required analyze resolution from the detector, a 2 ele list.
         # as a concequence, scale will be a 2 ele list
@@ -49,7 +47,7 @@ class ClassifyObject(Preview):
         # Convert pixels to numpy rgb
         rgba = np.fromstring(pixels, np.uint8).reshape(image_size[1],
                                                        image_size[0], 4)
-        rgb = rgba[:,:,:3]
+        rgb = rgba[:, :, :3]
         # detect
         detections = self.detector.detect(rgb)
         now = time.time()
@@ -62,14 +60,14 @@ class ClassifyObject(Preview):
             # Bounding box, pixels coordinates
             x = detection.bounding_box.left
             y = detection.bounding_box.top
-            w = detection.bounding_box.right - x 
+            w = detection.bounding_box.right - x
             h = detection.bounding_box.bottom - y
 
             # Map tflite style coordinates to Kivy Preview coordinates
-            y = max(image_size[1] -y -h, 0)
+            y = max(image_size[1] - y - h, 0)
             if mirror:
-                x = max(image_size[0] -x -w, 0)
-                
+                x = max(image_size[0] - x - w, 0)
+
             # Map Analysis Image coordinates to Preview coordinates
             # image_scale is a list because we used self.auto_analyze_resolution
             x = round(x * image_scale[0] + image_pos[0])
@@ -81,15 +79,15 @@ class ClassifyObject(Preview):
             category = detection.categories[0]
             class_name = category.label
             probability = round(category.score, 2)
-            result_text = class_name +\
-                ' (Probability: {:.2f} FPS: {:.1f} )'.format(probability,fps)
-            label = CoreLabel(font_size = sp(20))
+            result_text = class_name + \
+                          ' (Probability: {:.2f} FPS: {:.1f} )'.format(probability, fps)
+            label = CoreLabel(font_size=sp(20))
             label.text = result_text
             label.refresh()
 
             # Thread safe result
-            found.append({'x':x, 'y':y, 'w':w, 'h':h, 't': label.texture})
-        self.make_thread_safe(list(found)) ## A COPY of the list            
+            found.append({'x': x, 'y': y, 'w': w, 'h': h, 't': label.texture})
+        self.make_thread_safe(list(found))  ## A COPY of the list
 
     @mainthread
     def make_thread_safe(self, found):
@@ -98,15 +96,14 @@ class ClassifyObject(Preview):
     ################################
     # Canvas Update  - on UI Thread
     ################################
-        
+
     def canvas_instructions_callback(self, texture, tex_size, tex_pos):
         # Add the analysis annotations
-        Color(0,1,0,1)
+        Color(0, 1, 0, 1)
         for r in self.classified:
             # Draw box
-            Line(rectangle=(r['x'], r['y'], r['w'], r['h']), width = dp(1.5))
+            Line(rectangle=(r['x'], r['y'], r['w'], r['h']), width=dp(1.5))
             # Draw text
-            Rectangle(size = r['t'].size,
-                      pos = [r['x'] + dp(10), r['y'] + dp(10)],
-                      texture = r['t'])       
-
+            Rectangle(size=r['t'].size,
+                      pos=[r['x'] + dp(10), r['y'] + dp(10)],
+                      texture=r['t'])

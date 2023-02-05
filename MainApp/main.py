2c37b6a1590a9a70afbe3601801b18cf9cb8e027
DNIIBOY <daniel.nettelfield@gmail.com>
2023-02-06 00:51:47 +0100

    Weird (last) kivy thing

--- MainApp/main.py
+++ MainApp/main.py
@@ -1,20 +1,25 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.core.window import Window
 
 kivy.require("2.1.0")
 
 
-class ScaffoldList(BoxLayout):
+class MainWindow(TabbedPanel):
     def __init__(self):
-        super(ScaffoldList, self).__init__()
+        super(MainWindow, self).__init__()
 
 
 class Main(App):
     def build(self):
-        return ScaffoldList()
+        return MainWindow()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = Main()
     app.run()
